7daca8e45a19ad3e2c615d67f3becab803d40232
DREAD21 <87900013+DREAD21@users.noreply.github.com>
2021-08-25 15:42:18 +0300

    Add files via upload

---
+++ аэродром/Form1.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace аэродром
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox13 = new System.Windows.Forms.TextBox();
+            this.textBox14 = new System.Windows.Forms.TextBox();
+            this.textBox15 = new System.Windows.Forms.TextBox();
+            this.часы = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.textBox16 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(240, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "0";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(-188, 161);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(207, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(175, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "0";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Location = new System.Drawing.Point(394, 535);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(15, 13);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Location = new System.Drawing.Point(243, 367);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(15, 13);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Location = new System.Drawing.Point(303, 367);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(15, 13);
+            this.textBox4.TabIndex = 6;
+            // 
+            // textBox5
+            // 
+            this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox5.Location = new System.Drawing.Point(303, 239);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(15, 13);
+            this.textBox5.TabIndex = 7;
+            // 
+            // textBox6
+            // 
+            this.textBox6.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox6.Location = new System.Drawing.Point(303, 174);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(15, 13);
+            this.textBox6.TabIndex = 8;
+            // 
+            // textBox7
+            // 
+            this.textBox7.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox7.Location = new System.Drawing.Point(243, 535);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(15, 13);
+            this.textBox7.TabIndex = 9;
+            // 
+            // textBox8
+            // 
+            this.textBox8.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox8.Location = new System.Drawing.Point(243, 150);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(15, 13);
+            this.textBox8.TabIndex = 10;
+            // 
+            // textBox9
+            // 
+            this.textBox9.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox9.Location = new System.Drawing.Point(243, 296);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(15, 13);
+            this.textBox9.TabIndex = 11;
+            // 
+            // textBox10
+            // 
+            this.textBox10.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox10.Location = new System.Drawing.Point(197, -65);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(15, 13);
+            this.textBox10.TabIndex = 12;
+            // 
+            // textBox11
+            // 
+            this.textBox11.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox11.Location = new System.Drawing.Point(85, 296);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(15, 13);
+            this.textBox11.TabIndex = 13;
+            // 
+            // textBox12
+            // 
+            this.textBox12.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox12.Location = new System.Drawing.Point(85, 150);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(15, 13);
+            this.textBox12.TabIndex = 14;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(404, 471);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "стоп";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(404, 430);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 20;
+            this.button3.Text = "старт";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox13
+            // 
+            this.textBox13.Location = new System.Drawing.Point(12, 433);
+            this.textBox13.Name = "textBox13";
+            this.textBox13.Size = new System.Drawing.Size(44, 20);
+            this.textBox13.TabIndex = 21;
+            // 
+            // textBox14
+            // 
+            this.textBox14.Location = new System.Drawing.Point(85, 433);
+            this.textBox14.Name = "textBox14";
+            this.textBox14.Size = new System.Drawing.Size(45, 20);
+            this.textBox14.TabIndex = 22;
+            // 
+            // textBox15
+            // 
+            this.textBox15.Location = new System.Drawing.Point(157, 433);
+            this.textBox15.Name = "textBox15";
+            this.textBox15.Size = new System.Drawing.Size(50, 20);
+            this.textBox15.TabIndex = 23;
+            // 
+            // часы
+            // 
+            this.часы.AutoSize = true;
+            this.часы.Location = new System.Drawing.Point(12, 481);
+            this.часы.Name = "часы";
+            this.часы.Size = new System.Drawing.Size(44, 13);
+            this.часы.TabIndex = 25;
+            this.часы.Text = "часы    ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(85, 481);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 13);
+            this.label6.TabIndex = 26;
+            this.label6.Text = "минуты";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(157, 481);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(50, 13);
+            this.label7.TabIndex = 27;
+            this.label7.Text = "секунды";
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox16
+            // 
+            this.textBox16.Location = new System.Drawing.Point(69, 391);
+            this.textBox16.Name = "textBox16";
+            this.textBox16.Size = new System.Drawing.Size(84, 20);
+            this.textBox16.TabIndex = 28;
+            this.textBox16.Text = "Введите время";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::аэродром.Properties.Resources._1;
+            this.ClientSize = new System.Drawing.Size(491, 586);
+            this.Controls.Add(this.textBox16);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.часы);
+            this.Controls.Add(this.textBox15);
+            this.Controls.Add(this.textBox14);
+            this.Controls.Add(this.textBox13);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox13;
+        private System.Windows.Forms.TextBox textBox14;
+        private System.Windows.Forms.TextBox textBox15;
+        private System.Windows.Forms.Label часы;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox16;
+    }
+}
+
---
+++ аэродром/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/* Краснов Никита М80-203Б-19
+ * Лабораторная № 6 */
+namespace аэродром
+{
+    public partial class Form1 : Form
+    {
+        Timer timer = new Timer();
+        public int time;
+        public int s;
+        public int m;
+        public int h;
+        public bool day;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        
+        
+        private void Form1_load(object sender, EventArgs e)
+        {
+            timer.Interval = 1000;
+            timer.Tick += new EventHandler(timer1_Tick);
+            
+           
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+           
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            s = s + 1;
+            if (h > 0 && h < 12)
+            {
+                day = false;
+            }
+            if (h > 12 )
+            {
+                day = true;
+            }
+            if (s >= 60)
+            {
+                m = m + 1;
+                s = 0;
+            }
+            if (m >= 60)
+            {
+                h = h + 1;
+                m = 0;
+            }
+            if(h == 24)
+            {
+                h = 0;
+                m = 0;
+                s = 0;
+            }
+            label1.Text = s.ToString();
+            label2.Text = m.ToString();
+            label3.Text = h.ToString();
+            
+            if (h != -1)
+            {
+                textBox2.BackColor = Color.FromArgb(131, 209, 254);
+                textBox3.BackColor = Color.FromArgb(131, 209, 254);
+                textBox7.BackColor = Color.FromArgb(131, 209, 254);
+                textBox4.BackColor = Color.FromArgb(131, 209, 254);
+                textBox5.BackColor = Color.FromArgb(131, 209, 254);
+                textBox6.BackColor = Color.FromArgb(131, 209, 254);
+                textBox8.BackColor = Color.FromArgb(105, 255, 148);
+                textBox9.BackColor = Color.FromArgb(105, 255, 148);
+                textBox12.BackColor = Color.FromArgb(105, 255, 148);
+                textBox11.BackColor = Color.FromArgb(105, 255, 148);
+            }
+            if ((h >= 18 && m > 19 && day == true))
+            {
+                textBox2.BackColor = Color.FromArgb(63, 93, 255);
+                textBox3.BackColor = Color.FromArgb(63, 93, 255);
+                textBox7.BackColor = Color.FromArgb(63, 93, 255);
+                textBox4.BackColor = Color.FromArgb(63, 93, 255);
+                textBox5.BackColor = Color.FromArgb(63, 93, 255);
+                textBox6.BackColor = Color.FromArgb(63, 93, 255);
+                textBox8.BackColor = Color.FromArgb(0, 215, 15);
+                textBox9.BackColor = Color.FromArgb(0, 215, 15);
+                textBox12.BackColor = Color.FromArgb(0, 215, 15);
+                textBox11.BackColor = Color.FromArgb(0, 215, 15);
+
+            }
+            if ((h >= 18 && m > 39 && day == true))
+            {
+                textBox2.BackColor = Color.FromArgb(42, 38, 243);
+                textBox3.BackColor = Color.FromArgb(42, 38, 243);
+                textBox7.BackColor = Color.FromArgb(42, 38, 243);
+                textBox4.BackColor = Color.FromArgb(42, 38, 243);
+                textBox5.BackColor = Color.FromArgb(42, 38, 243);
+                textBox6.BackColor = Color.FromArgb(42, 38, 243);
+                textBox8.BackColor = Color.FromArgb(0, 138, 0);
+                textBox9.BackColor = Color.FromArgb(0, 138, 0);
+                textBox12.BackColor = Color.FromArgb(0, 138, 0);
+                textBox11.BackColor = Color.FromArgb(0, 138, 0);
+            }
+            if (h >= 19 && day ==true)
+            {
+                textBox2.BackColor = Color.FromArgb(22, 31, 151);
+                textBox3.BackColor = Color.FromArgb(22, 31, 151);
+                textBox7.BackColor = Color.FromArgb(22, 31, 151);
+                textBox4.BackColor = Color.FromArgb(22, 31, 151);
+                textBox5.BackColor = Color.FromArgb(22, 31, 151);
+                textBox6.BackColor = Color.FromArgb(22, 31, 151);
+                textBox8.BackColor = Color.FromArgb(0, 110, 0);
+                textBox9.BackColor = Color.FromArgb(0, 110, 0);
+                textBox12.BackColor = Color.FromArgb(0,110,0);
+                textBox11.BackColor = Color.FromArgb(0, 110, 0);
+            }
+            if (h >= 0 && day == false)
+            {
+                textBox2.BackColor = Color.FromArgb(22, 31, 151);
+                textBox3.BackColor = Color.FromArgb(22, 31, 151);
+                textBox7.BackColor = Color.FromArgb(22, 31, 151);
+                textBox4.BackColor = Color.FromArgb(22, 31, 151);
+                textBox5.BackColor = Color.FromArgb(22, 31, 151);
+                textBox6.BackColor = Color.FromArgb(22, 31, 151);
+                textBox8.BackColor = Color.FromArgb(0, 110, 0);
+                textBox9.BackColor = Color.FromArgb(0, 110, 0);
+                textBox12.BackColor = Color.FromArgb(0, 110, 0);
+                textBox11.BackColor = Color.FromArgb(0, 110, 0);
+            }
+            if (h >= 6 && day == false)
+            {
+                textBox2.BackColor = Color.FromArgb(42, 38, 243);
+                textBox3.BackColor = Color.FromArgb(42, 38, 243);
+                textBox7.BackColor = Color.FromArgb(42, 38, 243);
+                textBox4.BackColor = Color.FromArgb(42, 38, 243);
+                textBox5.BackColor = Color.FromArgb(42, 38, 243);
+                textBox6.BackColor = Color.FromArgb(42, 38, 243);
+                textBox8.BackColor = Color.FromArgb(0, 138, 0);
+                textBox9.BackColor = Color.FromArgb(0, 138, 0);
+                textBox12.BackColor = Color.FromArgb(0, 138, 0);
+                textBox11.BackColor = Color.FromArgb(0, 138, 0);
+            }
+            if ( h >= 6 && m >= 30 && day == false)
+            {
+                textBox2.BackColor = Color.FromArgb(63, 93, 255);
+                textBox3.BackColor = Color.FromArgb(63, 93, 255);
+                textBox7.BackColor = Color.FromArgb(63, 93, 255);
+                textBox4.BackColor = Color.FromArgb(63, 93, 255);
+                textBox5.BackColor = Color.FromArgb(63, 93, 255);
+                textBox6.BackColor = Color.FromArgb(63, 93, 255);
+                textBox8.BackColor = Color.FromArgb(0, 215, 15);
+                textBox9.BackColor = Color.FromArgb(0, 215, 15);
+                textBox12.BackColor = Color.FromArgb(0, 215, 15);
+                textBox11.BackColor = Color.FromArgb(0, 215, 15);
+            }
+            if (h >= 6 && m >= 50 && day == false)
+            {
+                textBox2.BackColor = Color.FromArgb(131, 209, 254);
+                textBox3.BackColor = Color.FromArgb(131, 209, 254);
+                textBox7.BackColor = Color.FromArgb(131, 209, 254);
+                textBox4.BackColor = Color.FromArgb(131, 209, 254);
+                textBox5.BackColor = Color.FromArgb(131, 209, 254);
+                textBox6.BackColor = Color.FromArgb(131, 209, 254);
+                textBox8.BackColor = Color.FromArgb(105, 255, 148);
+                textBox9.BackColor = Color.FromArgb(105, 255, 148);
+                textBox12.BackColor = Color.FromArgb(105, 255, 148);
+                textBox11.BackColor = Color.FromArgb(105, 255, 148);
+            }
+         
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+            label1.Text = "0";
+            label2.Text = "0";
+            label3.Text = "0";
+            timer.Enabled = false;
+            textBox13.Text = "";
+            textBox14.Text = "";
+            textBox15.Text = "";
+            textBox2.BackColor = Color.FromArgb(255, 255, 255);
+            textBox3.BackColor = Color.FromArgb(255, 255, 255);
+            textBox7.BackColor = Color.FromArgb(255, 255, 255);
+            textBox4.BackColor = Color.FromArgb(255, 255, 255);
+            textBox5.BackColor = Color.FromArgb(255, 255, 255);
+            textBox6.BackColor = Color.FromArgb(255, 255, 255);
+            textBox8.BackColor = Color.FromArgb(255, 255, 255);
+            textBox9.BackColor = Color.FromArgb(255, 255, 255);
+            textBox12.BackColor = Color.FromArgb(255, 255, 255);
+            textBox11.BackColor = Color.FromArgb(255, 255, 255);
+
+        }
+
+        
+        
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            h = Convert.ToInt32(textBox13.Text);
+            m = Convert.ToInt32(textBox14.Text);
+            s = Convert.ToInt32(textBox15.Text);
+            timer.Enabled = true;
+            timer.Start();
+        }
+    }
+}
